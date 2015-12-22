7f1d0d0933679ca2636b6ee167e4a1bf7c64e06e
AfroTok <boondarig@gmail.com>
2015-12-22 10:26:34 +0700

    ButtonController เสร็จแล้วววว

--- app/src/main/java/com/example/afrogunso/mytraffic/MainActivity.java
+++ app/src/main/java/com/example/afrogunso/mytraffic/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.afrogunso.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -20,7 +24,27 @@ public class MainActivity extends AppCompatActivity {
         //Bind Widget
         bindWidget();
 
+        //Button Contrller
+        buttonController();
+
+
     }   // Main Method
+
+    private void buttonController() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.dog);
+                buttonMediaPlayer.start();
+
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://youtu.be/mXig5cc9kNM"));
+                startActivity(objIntent);
+
+            }  //event
+        });
+    }
 
     private void bindWidget() {
 
